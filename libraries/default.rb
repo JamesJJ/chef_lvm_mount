de6911815ca9f6736c1fbf4b039869ca30c3cc65
JamesJJ <James_Hooper@htc.com>
2014-12-29 22:13:07 +0800

    bugs

--- libraries/default.rb
+++ libraries/default.rb
@@ -28,7 +28,9 @@ module Chef::Recipe::LVM_MOUNT
       next unless /\A\//.match(_m)
       Chef::Log.debug("Found mount: " + _m)
       _mountinfo = _m.split(' ')
-      _regex = Regexp.new('\A' + path + '[0-9]*\Z')
+      _regex = (/[0-9]\Z/.match(path)) ? 
+        Regexp.new('\A' + path + '[0-9]*\Z') :
+        Regexp.new('\A' + path + '\Z')
       Chef::Log.debug("Using mounted regex: " + _regex.inspect)
       return 1 if _regex.match(_mountinfo[0])
       return 2 if path==_mountinfo[1] 
