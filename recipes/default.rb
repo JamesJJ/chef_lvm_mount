2388aa4f392cb9377b44b8564251d249ef6c75ad
JamesJJ <James_Hooper@htc.com>
2014-12-29 21:36:55 +0800

    bugs

--- recipes/default.rb
+++ recipes/default.rb
@@ -20,22 +20,24 @@ node['lvm_mount']['disks'].each do |_disk|
     _pvexists = LVM_MOUNT.pvExists(_pv)
     execute "Initialising PV: #{_pv}" do
       command "pvcreate -y #{_pv}"
-      not_if _pvexists
+      not_if do _pvexists end
     end
     _pv_string << "#{_pv} " unless _pvexists
   end
   execute "Creating VG: #{_prefix}vg" do
     command "vgcreate -y #{_prefix}vg #{_pv_string}"
-    not_if LVM_MOUNT.vgExists(_prefix + 'vg')
+    not_if do LVM_MOUNT.vgExists(_prefix + 'vg') end
   end
   execute "Creating LV: #{_prefix}lv" do
     command "lvcreate -L #{_disk['size']} -n #{_prefix}lv #{_prefix}vg"
-    not_if LVM_MOUNT.lvExists(_prefix + 'lv')
+    not_if do LVM_MOUNT.lvExists(_prefix + 'lv') end
   end
   execute "Formating: /dev/#{_prefix}vg/#{_prefix}lv as #{_format}" do
     command "mkfs -t #{_format} /dev/#{_prefix}vg/#{_prefix}lv"
-    not_if LVM_MOUNT.isMounted("/dev/mapper/#{_prefix}vg-#{_prefix}lv") ||
-             LVM_MOUNT.isMounted("/dev/#{_prefix}vg/#{_prefix}lv")
+    not_if do 
+      LVM_MOUNT.isMounted("/dev/mapper/#{_prefix}vg-#{_prefix}lv") ||
+      LVM_MOUNT.isMounted("/dev/#{_prefix}vg/#{_prefix}lv")
+    end
   end
   mount _mountpoint do
     device "/dev/#{_prefix}vg/#{_prefix}lv"
